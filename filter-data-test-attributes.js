b209efa4e15ca57702748bdad0b763c0b75f9414
Kyle Mellander <kmellander@focusvision.com>
2016-09-01 22:06:17 -0700

    Use regex for more universal matching tdata-test attributes

--- filter-data-test-attributes.js
+++ filter-data-test-attributes.js
@@ -8,8 +8,7 @@ FilterDataTestAttributesTransform.prototype.transform = function(ast) {
   walker.visit(ast, function(node) {
     if (node.type === 'ElementNode') {
       node.attributes = node.attributes.filter(function(attribute) {
-        var name = attribute.name;
-        return !(name === 'data-test-id' || name === 'data-test-class');
+        return /^data-test/.test(attribute.name);
       });
     }
   });
