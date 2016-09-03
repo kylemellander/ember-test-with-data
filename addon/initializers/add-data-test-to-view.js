a9b224021011635213f767c5bb14fde136edfbab
Kyle Mellander <kmellander@focusvision.com>
2016-09-02 23:34:27 -0700

    Refactor intializer

--- addon/initializers/add-data-test-to-view.js
+++ addon/initializers/add-data-test-to-view.js
@@ -18,35 +18,39 @@ export default {
       'ember-test-with-data': addonOptions = {}
     } = ENV;
     const {
-      autoTag = true,
       hiddenEnvironments = ['production'],
-      dataTestSuffix
     } = addonOptions;
 
     if (hiddenEnvironments.indexOf(environment) !== -1) { return; }
 
-    const dasherizeAttr = dataTestSuffix ?
-      `data-test-${dasherize(dataTestSuffix)}` : 'data-test';
-    const camelizeAttr = camelize(dasherizeAttr);
-
-    const componentAttrs = {
-      attributeBindings: [`${camelizeAttr}:${dasherizeAttr}`]
-    };
-
-    componentAttrs[camelizeAttr] = computed.alias(dasherizeAttr);
-
-    if (autoTag) {
-      componentAttrs[dasherizeAttr] = computed('dataTestSuffix', function() {
-        const suffix = this.get('dataTestSuffix');
-        let baseId = (this._debugContainerKey || '')
-          .replace(/.*component:/g, '')
-          .replace(/\//g, '-')
-          .replace(/^-/, '');
-
-        return typeof suffix === 'undefined' ? baseId : `${baseId}-${suffix}`;
-      });
-    }
+    const componentAttrs = this._buildAttrs(addonOptions);
 
     Component.reopen(componentAttrs);
+  },
+
+  autoTagComputed: computed('dataTestSuffix', function() {
+    const suffix = this.get('dataTestSuffix');
+    let baseId = (this._debugContainerKey || '')
+      .replace(/.*component:/g, '')
+      .replace(/\//g, '-')
+      .replace(/^-/, '');
+
+    return typeof suffix === 'undefined' ? baseId : `${baseId}-${suffix}`;
+  }),
+
+  _buildAttrs({ dataTestSuffix, autoTag = true }) {
+    const attrs = {};
+    const dasherizeAttr = this._buildDasherizedAttr(dataTestSuffix);
+    const camelizeAttr = camelize(dasherizeAttr);
+
+    attrs.attributeBindings = [`${camelizeAttr}:${dasherizeAttr}`];
+    attrs[camelizeAttr] = computed.alias(dasherizeAttr);
+    if (autoTag) { attrs[dasherizeAttr] = this.autoTagComputed; }
+
+    return attrs;
+  },
+
+  _buildDasherizedAttr(suffix) {
+    return suffix ? `data-test-${dasherize(suffix)}` : 'data-test';
   }
 };
