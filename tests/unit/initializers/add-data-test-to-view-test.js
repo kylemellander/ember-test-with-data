d562d6d04d4d8ed889ee6b3cabb59a5cd871fd2f
Kyle Mellander <kmellander@focusvision.com>
2016-09-02 23:16:09 -0700

    Add tests for setting up components

--- tests/unit/initializers/add-data-test-to-view-test.js
+++ tests/unit/initializers/add-data-test-to-view-test.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
-import AddDataTestToViewInitializer from 'dummy/initializers/add-data-test-to-view';
+import AddDataTestToViewInitializer
+  from 'dummy/initializers/add-data-test-to-view';
 import { module, test } from 'qunit';
 
 let application;
@@ -13,10 +14,37 @@ module('Unit | Initializer | add data test to view', {
   }
 });
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('adds classBinding for data-test attribute', assert => {
+  assert.expect(3);
+
+  const config = {};
+  application.register('config:environment', config);
+
+  const Component = Ember.Component.extend();
+  application.register('component:test-component', Component);
+
   AddDataTestToViewInitializer.initialize(application);
 
-  // you would normally confirm the results of the initializer here
-  assert.ok(true);
+  const component = Component.create();
+  component._debugContainerKey = 'component:test-component';
+  const { attributeBindings } = component;
+
+  assert.ok(
+    attributeBindings.indexOf('dataTest:data-test') !== -1,
+    'attributeBindings include data-test identifiers'
+  );
+
+  assert.equal(
+    component.get('dataTest'),
+    'test-component',
+    'data-test attributes is name of component by default'
+  );
+
+  component.set('dataTestSuffix', 'lucky-charms');
+
+  assert.equal(
+    component.get('dataTest'),
+    'test-component-lucky-charms',
+    'data-test attributes is name of component -dataTestSuffix'
+  );
 });
