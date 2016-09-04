47abd5b2b8e781399c01577f70d3399721762dd8
Kyle Mellander <kmellander@focusvision.com>
2016-09-04 16:33:14 -0700

    Add tests for other autotagging

--- tests/acceptance/auto-tag-test.js
+++ tests/acceptance/auto-tag-test.js
@@ -14,3 +14,21 @@ test('link-to tag gets autoTagged', assert => {
 
   andThen(() => assert.ok(findWithData('link-to').is('a')));
 });
+
+test('input helper gets autoTagged', assert => {
+  visit('/auto-tag');
+
+  andThen(() => assert.ok(findWithData('text-field').is('input')));
+});
+
+test('textarea helper gets autoTagged', assert => {
+  visit('/auto-tag');
+
+  andThen(() => assert.ok(findWithData('text-area').is('textarea')));
+});
+
+test('custom component gets autoTagged', assert => {
+  visit('/auto-tag');
+
+  andThen(() => assert.ok(findWithData('data-test').is('div')));
+});
