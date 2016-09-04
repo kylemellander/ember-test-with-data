eff300a162f233ea07a5bb089f739f7f6e82d774
Kyle Mellander <kmellander@focusvision.com>
2016-09-03 17:30:30 -0700

    Add tests for initializer

--- tests/unit/initializers/add-data-test-to-view-test.js
+++ tests/unit/initializers/add-data-test-to-view-test.js
@@ -3,21 +3,32 @@ import AddDataTestToViewInitializer
   from 'dummy/initializers/add-data-test-to-view';
 import { module, test } from 'qunit';
 
-let application;
+const {
+  Application,
+  run
+} = Ember;
+
+let application, component;
 
 module('Unit | Initializer | add data test to view', {
   beforeEach() {
-    Ember.run(function() {
-      application = Ember.Application.create();
-      application.deferReadiness();
+    run(function() {
+      application = Application.create();
+    });
+  },
+
+  afterEach() {
+    run(() => {
+      if (application) {
+        application.destroy();
+        application = null;
+        component = null;
+      }
     });
   }
 });
 
-test('adds classBinding for data-test attribute', assert => {
-  assert.expect(3);
-
-  const config = {};
+const initializeComponent = (config = {}) => {
   application.register('config:environment', config);
 
   const Component = Ember.Component.extend();
@@ -25,26 +36,42 @@ test('adds classBinding for data-test attribute', assert => {
 
   AddDataTestToViewInitializer.initialize(application);
 
-  const component = Component.create();
+  component = Component.create();
   component._debugContainerKey = 'component:test-component';
-  const { attributeBindings } = component;
+};
 
-  assert.ok(
-    attributeBindings.indexOf('dataTest:data-test') !== -1,
-    'attributeBindings include data-test identifiers'
-  );
+test('not set on production', assert => {
+  initializeComponent({ environment: 'production' });
 
-  assert.equal(
-    component.get('dataTest'),
-    'test-component',
-    'data-test attributes is name of component by default'
-  );
+  assert.equal(component.attributeBindings.indexOf('dataTest:data-test'), -1);
+});
 
+test('not set when hiddenEnv matches env', assert => {
+  initializeComponent({
+    environment: 'staging',
+    'ember-test-with-data': {
+      hiddenEnvironments: ['production', 'staging']
+    }
+  });
+
+  assert.equal(component.attributeBindings.indexOf('dataTest:data-test'), -1);
+});
+
+test('adds attributeBindings for data-test attribute', assert => {
+  initializeComponent();
+
+  assert.ok(component.attributeBindings.indexOf('dataTest:data-test') !== -1);
+});
+
+test('data-test attributes is name of component by default', assert => {
+  initializeComponent();
+
+  assert.equal(component.get('dataTest'), 'test-component');
+});
+
+test('data-test attributes is name of component -dataTestSuffix', assert => {
+  initializeComponent();
   component.set('dataTestSuffix', 'lucky-charms');
 
-  assert.equal(
-    component.get('dataTest'),
-    'test-component-lucky-charms',
-    'data-test attributes is name of component -dataTestSuffix'
-  );
+  assert.equal(component.get('dataTest'), 'test-component-lucky-charms');
 });
