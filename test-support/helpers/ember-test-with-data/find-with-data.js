f1c97edae76044af88429558da92ae6cdafde92f
Kyle Mellander <kmellander@focusvision.com>
2016-09-04 14:59:15 -0700

    add acceptance test

---
+++ test-support/helpers/ember-test-with-data/find-with-data.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+const { Test: { registerHelper } } = Ember;
+
+export default registerHelper('findWithData', (app, value = '') => {
+  const config = app.resolveRegistration('config:ember-test-with-data');
+  const attr = config.attr;
+  const selector = value ? `[${attr}=${value}]` : `[${attr}]`;
+  return find(selector);
+});
