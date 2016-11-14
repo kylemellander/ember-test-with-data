da40e7755117fefacb9031fe37cad1ef90047e61
Kashif Khan <kkhan@multiservice.com>
2016-11-15 09:15:12 +1100

    Fix #9: Support ember LTS 2.4.x and node older versions

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
 var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-test-with-data',
-  included(app) {
+  included: function(app) {
     var registry = app.registry;
     var options = app.options || {};
     this.env = app.env;
+
     var configPath = options.configPath;
+
+
+    // NOTE: For ember-cli >= 2.6.0-beta.3, project.configPath() returns absolute path
+    // while older ember-cli versions return path relative to project root
+    if (!path.isAbsolute(configPath)) {
+      configPath = path.join(app.project.root, configPath);
+    }
+
     if (!this.env || !configPath) { return; }
 
     var config = require(configPath)(this.env) || {};
@@ -27,9 +37,9 @@ module.exports = {
     }
   },
 
-  treeForAddon() {
+  treeForAddon: function() {
     var tree = this._super.treeForAddon.apply(this, arguments);
-    const hiddenEnvironments = this.hiddenEnvironments || ['production'];
+    var hiddenEnvironments = this.hiddenEnvironments || ['production'];
     if (hiddenEnvironments.indexOf(this.env) !== -1) {
       this.ui.writeLine('Stripping all data test attributes');
       tree = new Funnel(tree, { exclude: [ /add-data-test-to-view/ ] });
